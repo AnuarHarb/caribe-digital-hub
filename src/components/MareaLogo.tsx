4129fb2df94bdafe1308254d99fd080fa3288b13
Anuar Harb <anuarharb@gmail.com>
2026-09-09 23:42:13 -0500

    la marea logo black

--- src/components/MareaLogo.tsx
+++ src/components/MareaLogo.tsx
@@ -1,11 +1,13 @@
 import { cn } from "@/lib/utils";
 
+const OSCURO = "/logos/la-marea-black.png";
+const CLARO = "/logos/la-marea.png";
+
 export function MareaLogo({ className }: { className?: string }) {
   return (
-    <img
-      src="/logos/la-marea.png"
-      alt="La Marea"
-      className={cn("w-auto", className)}
-    />
+    <span className={cn("inline-flex items-center", className)}>
+      <img src={OSCURO} alt="La Marea" className="h-full w-auto dark:hidden" />
+      <img src={CLARO} alt="" className="hidden h-full w-auto dark:block" aria-hidden />
+    </span>
   );
 }
